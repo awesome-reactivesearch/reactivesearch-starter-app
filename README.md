51aac16012e4fee1c3268ea8ffa2f0a82ff601d4
Siddharth Kothari <sids.aquarius@gmail.com>
2017-12-25 21:55:31 +0530

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+### Reactivesearch Starter App
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+Step-by-step guide available at [ReactiveSearch Quickstart Doc](https://opensource.appbase.io/reactive-manual/getting-started/reactivesearch.html).
+
+### Develop
+
+```
+yarn && yarn start
+```
+
+should open something like this
+
+![](https://i.imgur.com/Zgp5lGk.png)
+
+
+### Configure
+
+The ReactiveSearch components code resides in `src/App.js` file. For building this app, we use:
+
+1. [appbase.io](https://appbase.io) for the backend, any Elasticsearch cluster/index should work.  
+2. Flex based layout system from [reflexbox](https://github.com/jxnblk/reflexbox), you can use Materialize's or Bootstrap's grid, or roll your own layout - the ReactiveSearch components are layout agnostic.  
+3. The following components:
+ - **ReactiveBase** - ReactiveBase is the provider component that connects the UI with the backend app.
+ - **CategorySearch** - CategorySearch component provides a search box UI with categorized suggestions.
+ - **SingleRange** - SingleRange component is used for displaying the star ratings.
+ - **ResultCard** - ResultCard component is used for displaying the **hits** as a card layout.
---
+++ src/App.js
@@ -0,0 +1,68 @@
+const { ReactiveBase, CategorySearch, SingleRange, ResultCard } = ReactiveSearch;
+
+class App extends React.Component {
+  render() {
+    return (
+        <ReactiveBase
+        app="car-store"
+        credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c">
+          <div style={{ display: "flex", "flexDirection": "row" }}>
+            <div style={{ display: "flex", "flexDirection": "column", "width": "40%" }}>
+              <CategorySearch
+                componentId="searchbox"
+                dataField="name"
+                categoryField="brand.raw"
+                placeholder="Search for cars"
+                style={{
+                  padding: "5px",
+                  "marginTop": "10px"
+                }}
+              />
+              <SingleRange
+                componentId="ratingsfilter"
+                title="Filter by ratings"
+                dataField="rating"
+                data={[
+                  {"start": "4", "end": "5", "label": "4 stars and up"},
+                  {"start": "3", "end": "5", "label": "3 stars and up"},
+                  {"start": "2", "end": "5", "label": "2 stars and up"},
+                  {"start": "1", "end": "5", "label": "see all ratings"},
+                ]}
+                defaultSelected="4 stars and up"
+                style={{
+                  padding: "5px",
+                  "marginTop": "10px"
+                }}
+              />
+            </div>
+            <ResultCard
+              componentId="result"
+              title="Results"
+              from={0}
+              size={6}
+              pagination={true}
+              react={{
+                and: ["searchbox", "ratingsfilter"]
+              }}
+              onData={(res) => {
+                return {
+                  image: "https://www.enterprise.com/content/dam/global-vehicle-images/cars/FORD_FOCU_2012-1.png",
+                  title: res.name,
+                  desc: res.brand + " " + "★".repeat(res.rating)
+                }
+              }}
+              style={{
+                "width": "60%",
+                "textAlign": "center"
+              }}
+            />
+          </div>
+        </ReactiveBase>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App></App>,
+  document.getElementById('root')
+);
