3a257dbbcd6bd2610c226cb8479a09b09f330d91
Siddharth Kothari <sids.aquarius@gmail.com>
2018-01-06 22:21:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
             </div>
             <ResultCard
               componentId="result"
+              dataField="name"
               title="Results"
               from={0}
               size={6}
